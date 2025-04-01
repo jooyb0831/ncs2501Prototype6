2d039761d1b703494ee14c6cfb86e440b38705d8
jooyb0831 <dia231@naver.com>
2025-04-01 15:48:56 +0900

    0401

--- Assets/Optimization/Scripts/OptimUnit.cs
+++ Assets/Optimization/Scripts/OptimUnit.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Profiling;
@@ -31,11 +32,15 @@ public class OptimUnit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Profiler.BeginSample("Handling Time");
         HandleTime();
+        Profiler.EndSample();
 
+        Profiler.BeginSample("Rotating");
         var t = transform;
         float xRotation = currentAngularVelocity * Time.deltaTime;
         float zRotation = currentAngularVelocity * Time.deltaTime;
+
 
         if (transform.position.x > 0)
             xRotation *= -1;
@@ -44,9 +49,13 @@ public class OptimUnit : MonoBehaviour
             zRotation *= -1;
 
         transform.Rotate(xRotation, 0, zRotation);
+        Profiler.EndSample();
         
+        Profiler.BeginSample("Move");
         Move();
+        Profiler.EndSample();
 
+        Profiler.BeginSample("B_check");
         //check if we are moving away from the zone and invert velocity if this is the case
         if (transform.position.x > areaSize.x && currentVelocity.x > 0
             || (transform.position.x < -areaSize.x && currentVelocity.x < 0))
@@ -61,6 +70,7 @@ public class OptimUnit : MonoBehaviour
             currentVelocity.z *= -1;
             PickNewVelocityChangeTime(); //we pick a new change time as we changed velocity
         }
+        Profiler.EndSample();
     }
 
 
@@ -88,6 +98,7 @@ public class OptimUnit : MonoBehaviour
 
     void Move()
     {
+        /*
         Vector3 position = transform.position;
         
         float distanceToCenter = Vector3.Distance(Vector3.zero, position);
@@ -101,6 +112,8 @@ public class OptimUnit : MonoBehaviour
         }
         
         transform.position = position;
+        */
+        transform.position = transform.position + currentVelocity * Time.deltaTime;
     }
 
     private void HandleTime()
